e503b51473a07e5fb2276d01cb3779ab7d75f6c4
hinanish <167354026+hinanish@users.noreply.github.com>
2024-10-21 00:16:47 +0500

    changes

--- src/app/about/reviews/page.tsx
+++ src/app/about/reviews/page.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import css from './reviews.module.css'
+import React from 'react';
+import css from './reviews.module.css';
 
 export default function Review() {
   return (
-    <div> <h1 className={css.h1css}> Reviw page</h1> <span className={css.css}><p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, hic.</p>
-    </span>  <footer className={css.footer}>
-     <span>hello</span> <span>i'm a footer</span><p>&copy;2024 Your business. All rights reserved.</p>
-
-    </footer></div>
-    
-  )
+    <div>
+      <h1 className={css.h1css}>Review page</h1>
+      <span className={css.css}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, hic.</p>
+      </span>
+      <footer className={css.footer}>
+        <span>hello</span> 
+        <span>i&#39;m a footer</span> 
+        <p>&copy; 2024 Your business. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
